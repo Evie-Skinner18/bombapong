602b2d783c1fa5efcb735e767ef42a822fb4502c
Evie <evie.skinner@e-days.com>
2020-11-13 18:03:12 +0000

    Removed conn string oops

--- api/messageModel.js
+++ api/messageModel.js
@@ -1,6 +1,3 @@
 const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema()
-
-// put real password in env file to replace <password> or do the Azure configuration
-const connectionString = 'mongodb+srv://Eves:<password>@eviescluster.25kgo.mongodb.net/<dbname>?retryWrites=true&w=majority';
